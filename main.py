cd1aa014a8f9183562f50de36b78830d0a2c7cbc
BombasticTom <bigolgamingbag@gmail.com>
2023-04-21 17:45:47 +0200

    Day 2!

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-# Make a discord bot that should order a pizza using shadows (ShadowMario's) debit card, delivered straight to their door - Riveren 2023
+# DAY 1: Make a discord bot that should order a pizza using shadows (ShadowMario's) debit card, delivered straight to their door - Riveren 2023
+# DAY 2: Make it send a spooky image every time someone with #SHADOWCULT in their name sends a message - Riveren 2023
 
 import discord
 import os
@@ -7,34 +8,39 @@ import random
 from discord.ext import commands
 from source.Configuration import Configuration
 from source.Humor import Humor
+from source.Paths import Paths
 
 class Client(commands.Bot):
     # A lil class i made for managing the bot easier
     def __init__(self, cmdprf: str, dcIntents: discord.Intents):
         # Initializing stuff (not important)
         super().__init__(command_prefix = cmdprf, intents = dcIntents())
-        # self.cogsToLoad = [file[:-3] for file in os.listdir(os.getcwd() + '/source/cogs') if file.endswith('.py')]
+        self.cogsToLoad = [file[:-3] for file in os.listdir('./source/cogs') if file.endswith('.py')]
 
-    """
     async def setup_hook(self):
-        # Loads cogs (commented out since there's currently none)
+        # Loads cogs
         for ext in self.cogsToLoad:
             await self.load_extension(f'source.cogs.{ext}')
-    """
 
     async def on_ready(self):
         # Notifies the owner that the client is ready for use
-        print(f'{client.user.name} is ready to go!')
+        print(f'{client.user} is ready to go!')
 
     async def on_message(self, message: discord.Message):
         if message.author.bot: return # Verifies if the message sender isn't the client itself
         # Checks if your bot is mentioned in the message context, including words "order pizza"
         if self.user.mentioned_in(message=message) and "order pizza" in message.content.lower():
-            # Initiate funny.
+            # Initiate funny 🤣!
             await message.reply(
                 f"Just ordered pizza using ShadowMario's {abs(random.randrange(0, 100, 10)-1)}.99$ {random.choice(Humor.popularPlatforms)} "
                 f"giftcard! The code was `{Humor.giftcardGenerator()}`.\n"
                 f"The delivery has been scheduled. I am going to find you in... {Humor.formatTime(random.randint(1, 7201))}!"
+            )
+        # Checks if you're in ShadowMario's cult and if you've mentioned riveren.
+        if 'riveren' in message.content.lower() and '#shadowcult' in message.author.display_name.lower():
+            # Initiate scary 😱!
+            await message.reply(
+                "Boo! Did I scare you?! 😹", file = discord.File(Paths.image("riveren"))
             )
 
 # Running the custom Bot Client class
--- source/Paths.py
+++ source/Paths.py
@@ -1,9 +1,9 @@
-# A custom Paths text wrapper (won't explain how it works since it's unused).
+# A custom Paths text wrapper
 
 class Paths:
-    assetsDir = 'assets/'
+    assetsDir = 'assets/' # Default folder where assets will be stored.
 
-    @staticmethod 
+    @staticmethod
     def getPath(file:str, library:str = None):
         if library != None:
             return Paths.getLibraryPath(file, library)
@@ -17,6 +17,11 @@ class Paths:
     getLibraryPathForce = staticmethod(lambda file, library : f'{Paths.assetsDir}{library}/{file}')
     getPreloadPath = staticmethod(lambda file : f'{Paths.assetsDir}{file}')
 
-    txt = staticmethod(lambda key, library: Paths.getPath(f'{key}.txt', library))
-    xml = staticmethod(lambda key, library: Paths.getPath(f'{key}.xml', library))
-    json = staticmethod(lambda key, library: Paths.getPath(f'{key}.json', library))
+    # Some file types that it can search (text, xml, json and images).
+    # eg. If you want to get path of an image stored in "assets/images", use Paths.image("image-name")
+    # Or if you want to find from another location "assets/folder-name": Paths.image("image-name", "folder-name")
+
+    txt = staticmethod(lambda key, library = None: Paths.getPath(f'{key}.txt', library))
+    xml = staticmethod(lambda key, library = None: Paths.getPath(f'{key}.xml', library))
+    json = staticmethod(lambda key, library = None: Paths.getPath(f'{key}.json', library))
+    image = staticmethod(lambda key, library = None: Paths.getPath(f'images/{key}.png', library))
